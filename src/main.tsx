d68a23d6ef2fd3d8bd9097101f534dc814adf959
Stefan Todorov <bgtrak@gmail.com>
2024-02-23 22:05:53 +0200

    Add @clerk/themes package and update ClerkProvider appearance

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import "./index.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import { ThemeProvider } from "./theme-provider.tsx";
 import { ClerkProvider } from "@clerk/clerk-react";
+import { dark } from "@clerk/themes";
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 if (!PUBLISHABLE_KEY) {
@@ -15,7 +16,10 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
         <Router>
             <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-                <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+                <ClerkProvider
+                    publishableKey={PUBLISHABLE_KEY}
+                    appearance={{ baseTheme: dark, signIn: {baseTheme: dark}, signUp: {baseTheme: dark}}}
+                >
                     <App />
                 </ClerkProvider>
             </ThemeProvider>
